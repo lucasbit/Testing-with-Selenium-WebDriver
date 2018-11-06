a7ac3fab63556c51f390c4e966772ae5ac17c6bd
lucasbit <lukaszlinka1984@gmail.com>
2018-11-06 09:24:01 +0100

    Login script

---
+++ done/test3.js
@@ -0,0 +1,29 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const path = require('chromedriver').path;
+const chrome = require('selenium-webdriver/chrome');
+const webdriver = require('selenium-webdriver');
+const assert = require('chai').assert;
+var faker = require('faker');
+
+describe('Testing 12', function(){
+this.timeout(500000);
+
+beforeEach(async function(){
+    driver = await new Builder().forBrowser('chrome').build();
+    driver.get('http://localhost/litecart/admin/');
+    
+});
+
+afterEach(function(){
+    console.log('Test is done');
+     driver.quit();
+    
+});
+
+it('testing login', async function(){
+    await driver.findElement(By.css('#box-login input[name=username]')).sendKeys('admin');
+    await driver.findElement(By.css('#box-login input[name=password]')).sendKeys('admin');
+    await driver.findElement(By.css('#box-login button[name=login]')).click();
+});
+
+});
