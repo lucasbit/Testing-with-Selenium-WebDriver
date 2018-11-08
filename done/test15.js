685026cdc43daed3ec76e3e2d571a7d77a514303
lucasbit <lukaszlinka1984@gmail.com>
2018-11-08 09:14:34 +0100

    task 7 changed to nested asynchronous loops

--- done/test15.js
+++ done/test15.js
@@ -1,15 +1,15 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-const path = require('chromedriver').path;
+const path = require('geckodriver').path;
 const chrome = require('selenium-webdriver/chrome');
 const webdriver = require('selenium-webdriver');
 const assert = require('chai').assert;
 var faker = require('faker');
 
 describe('Testing 15', function(){
+driver = new Builder().forBrowser('firefox').build();
 this.timeout(500000);
 
 beforeEach(async function(){
-    driver = await new Builder().forBrowser('chrome').build();
     driver.get('http://localhost/litecart/admin/');
     await driver.findElement(By.css('#box-login input[name=username]')).sendKeys('admin');
     await driver.findElement(By.css('#box-login input[name=password]')).sendKeys('admin');
@@ -17,22 +17,28 @@ beforeEach(async function(){
     await driver.wait(until.elementLocated(By.css('#box-apps-menu li:nth-child(2)')),5000).click();
     await driver.wait(until.elementLocated(By.css('#doc-catalog')),5000).click();
     await driver.wait(until.elementLocated(By.css('.dataTable tr:nth-child(3) a')),5000).click();
+    
 });
 
-afterEach(function(){
+afterEach( function(){
     console.log('Test is done');
-     driver.quit();  
+    // driver.quit();  
 });
 
 it('Testing product carts in admin panel - loop', async function(){
     let products = await driver.findElements(By.css('.dataTable tr:nth-child(n + 5) td:nth-child(3) > a'));
-    await products.forEach( async (element)=> {
+ 
+    await products.map( async (element)=> {
+        
         //click on the element
-        await element.click();
+         element.click();
+         
         //get back to previous page
         await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+        
         //wait until previous page is loaded (and it should continue)
         await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+        products = await driver.findElements(By.css('.dataTable tr:nth-child(n + 5) td:nth-child(3) > a'));
         // getting logs
         driver.manage().logs().get("browser").then(function(logsEntries) {
             logsEntries.forEach(function(l) {
@@ -42,60 +48,60 @@ it('Testing product carts in admin panel - loop', async function(){
        })
    
 });
-it('Testing product carts in admin panel - manual duck1', async function(){
-    let duck = await driver.findElement(By.css('.dataTable tr:nth-child(5) a'));
-    await duck.click();
-    await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
-    await driver.manage().logs().get("browser").then(function(logsEntries) {
-        logsEntries.forEach(function(el) {
-            console.log(el)
-        });
-    });
-});
-it('Testing product carts in admin panel - manual duck2', async function(){
-    let duck = await driver.findElement(By.css('.dataTable tr:nth-child(6) a'));
-    await duck.click();
-    await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
-    await driver.manage().logs().get("browser").then(function(logsEntries) {
-        logsEntries.forEach(function(el) {
-            console.log(el)
-        });
-    });
-});
-it('Testing product carts in admin panel - manual duck3', async function(){
-    let duck = await driver.findElement(By.css('.dataTable tr:nth-child(7) a'));
-    await duck.click();
-    await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
-    await driver.manage().logs().get("browser").then(function(logsEntries) {
-        logsEntries.forEach(function(el) {
-            console.log(el)
-        });
-    });
-});
-it('Testing product carts in admin panel - manual duck4', async function(){
-    let duck = await driver.findElement(By.css('.dataTable tr:nth-child(8) a'));
-    await duck.click();
-    await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
-    await driver.manage().logs().get("browser").then(function(logsEntries) {
-        logsEntries.forEach(function(el) {
-            console.log(el)
-        });
-    });
-});
-it('Testing product carts in admin panel - manual duck5', async function(){
-    let duck = await driver.findElement(By.css('.dataTable tr:nth-child(9) a'));
-    await duck.click();
-    await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
-    await driver.manage().logs().get("browser").then(function(logsEntries) {
-        logsEntries.forEach(function(el) {
-            console.log(el)
-        });
-    });
-});
+// it('Testing product carts in admin panel - manual duck1', async function(){
+//     let duck = await driver.findElement(By.css('.dataTable tr:nth-child(5) a'));
+//     await duck.click();
+//     await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+//     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+//     await driver.manage().logs().get("browser").then(function(logsEntries) {
+//         logsEntries.forEach(function(el) {
+//             console.log(el)
+//         });
+//     });
+// });
+// it('Testing product carts in admin panel - manual duck2', async function(){
+//     let duck = await driver.findElement(By.css('.dataTable tr:nth-child(6) a'));
+//     await duck.click();
+//     await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+//     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+//     await driver.manage().logs().get("browser").then(function(logsEntries) {
+//         logsEntries.forEach(function(el) {
+//             console.log(el)
+//         });
+//     });
+// });
+// it('Testing product carts in admin panel - manual duck3', async function(){
+//     let duck = await driver.findElement(By.css('.dataTable tr:nth-child(7) a'));
+//     await duck.click();
+//     await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+//     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+//     await driver.manage().logs().get("browser").then(function(logsEntries) {
+//         logsEntries.forEach(function(el) {
+//             console.log(el)
+//         });
+//     });
+// });
+// it('Testing product carts in admin panel - manual duck4', async function(){
+//     let duck = await driver.findElement(By.css('.dataTable tr:nth-child(8) a'));
+//     await duck.click();
+//     await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+//     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+//     await driver.manage().logs().get("browser").then(function(logsEntries) {
+//         logsEntries.forEach(function(el) {
+//             console.log(el)
+//         });
+//     });
+// });
+// it('Testing product carts in admin panel - manual duck5', async function(){
+//     let duck = await driver.findElement(By.css('.dataTable tr:nth-child(9) a'));
+//     await duck.click();
+//     await driver.wait(until.elementLocated(By.css('button[name=cancel]')),5000).click();
+//     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog&category_id=1'),5000);
+//     await driver.manage().logs().get("browser").then(function(logsEntries) {
+//         logsEntries.forEach(function(el) {
+//             console.log(el)
+//         });
+//     });
+// });
 
 });
--- done/test7.js
+++ done/test7.js
@@ -1,137 +1,42 @@
-
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const path = require('chromedriver').path;
 const chrome = require('selenium-webdriver/chrome');
 const webdriver = require('selenium-webdriver');
+const assert = require('chai').assert;
+var faker = require('faker');
 
-describe('Testing 7', function(){
+describe('Testing 15', function(){
 this.timeout(500000);
 
 beforeEach(async function(){
     driver = await new Builder().forBrowser('chrome').build();
-    driver.get('http://localhost/litecart/admin/login.php');
+    driver.get('http://localhost/litecart/admin/');
+    await driver.findElement(By.css('#box-login input[name=username]')).sendKeys('admin');
+    await driver.findElement(By.css('#box-login input[name=password]')).sendKeys('admin');
+    await driver.findElement(By.css('#box-login button[name=login]')).click();
 });
 
 afterEach(function(){
-    // driver.quit();
-    console.log('ssss');
+    console.log('Test is done');
+     driver.quit();  
 });
 
-it('login', async function(){
-
-    //log to admin panel
-   await driver.findElement(By.css('#box-login > form > div.content > div:nth-child(2) > div > input')).sendKeys('admin');
-   await driver.findElement(By.css('#box-login > form > div.content > div:nth-child(3) > div > input')).sendKeys('admin');
-   await driver.findElement(By.css('#box-login > form > div.footer > button')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/'), 5000).catch(function(error){
-        console.log(error);
+it('Testing product carts in admin panel - loop',async function(){
+    let products = await driver.findElements(By.css('#app-'));
+        for (let i = 0; i < products.length;i++) {
+            products = await driver.findElements(By.css('#app-'));
+            await products[i].click();
+            await driver.wait(until.elementLocated(By.css('h1')),5000);
+            let nestedItems = await driver.findElements(By.css('.docs li'));
+            if (nestedItems.length > 0) {
+                for(let j = 0; j < nestedItems.length; j++) {
+                     nestedItems = await driver.findElements(By.css('.docs li'));
+                    await nestedItems[j].click();
+                    await driver.wait(until.elementLocated(By.css('h1')),5000);
+                }
+            }
+            await driver.wait(until.elementLocated(By.css('.logotype')),5000).click();
+            await driver.wait(until.urlIs('http://localhost/litecart/admin/'),5000);   
+        }
     });
-    //check option Apperance and its children
-    await driver.findElement(By.css('#app-appearance > a > span.name')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=appearance&doc=template'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-template')).click();
-    await driver.findElement(By.css('#doc-logotype')).click();
-    await driver.findElement(By.css('h1')).getText().then(function(text){
-        console.log("Text from main page is" + text)
-    });
-
-    //check option Catalog and its children
-    await driver.findElement(By.css('#app-catalog > a > span.name')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=catalog&doc=catalog'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-product_groups')).click();
-    await driver.findElement(By.css('#doc-option_groups')).click();
-    await driver.findElement(By.css('#doc-manufacturers')).click();
-    await driver.findElement(By.css('#doc-suppliers')).click();
-    await driver.findElement(By.css('#doc-delivery_statuses')).click();
-    await driver.findElement(By.css('#doc-sold_out_statuses')).click();
-    await driver.findElement(By.css('#doc-quantity_units')).click();
-    await driver.findElement(By.css('#doc-csv')).click();
-   //check Countries and its children
-    await driver.findElement(By.css('#app-countries')).click();
-    //check Countries and its children
-    await driver.findElement(By.css('#app-currencies')).click();
-    //check Coustomers and its children
-    await driver.findElement(By.css('#app-customers')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=customers&doc=customers'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-customers')).click();
-    await driver.findElement(By.css('#doc-csv')).click();
-    await driver.findElement(By.css('#doc-newsletter')).click();
-    //check GeoZones and its children
-    await driver.findElement(By.css('#app-geo_zones')).click();
-    //check Languages and its children
-    await driver.findElement(By.css('#app-languages')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=languages&doc=languages'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-languages')).click();
-    await driver.findElement(By.css('#doc-storage_encoding')).click();
-    //check Modules and its children
-    await driver.findElement(By.css('#app-modules')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=modules&doc=customer'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-customer')).click();
-    await driver.findElement(By.css('#doc-shipping')).click();
-    await driver.findElement(By.css('#doc-payment')).click();
-    await driver.findElement(By.css('#doc-order')).click();
-    await driver.findElement(By.css('#doc-order_total')).click();
-    await driver.findElement(By.css('#doc-jobs')).click();
-    //check Orders and its children
-    await driver.findElement(By.css('#app-orders')).click();
-    await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=orders&doc=orders'), 3000).catch(function(error){
-        console.log(error);
-    });
-    await driver.findElement(By.css('#doc-orders')).click();
-    await driver.findElement(By.css('#doc-order_statuses')).click();
-     //check Pages and its children
-     await driver.findElement(By.css('#app-pages')).click();
-     await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=pages&doc=pages'), 3000).catch(function(error){
-         console.log(error);
-     });
-     await driver.findElement(By.css('#doc-pages')).click();
-     await driver.findElement(By.css('#doc-csv')).click();
-      //check Reports and its children
-      await driver.findElement(By.css('#app-reports')).click();
-      await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=reports&doc=monthly_sales'), 3000).catch(function(error){
-          console.log(error);
-      });
-      await driver.findElement(By.css('#doc-monthly_sales')).click();
-      await driver.findElement(By.css('#doc-most_sold_products')).click();
-      await driver.findElement(By.css('#doc-most_shopping_customers')).click();
-      //check Settings and its children
-      await driver.findElement(By.css('#app-settings')).click();
-      await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=settings&doc=store_info'), 3000).catch(function(error){
-          console.log(error);
-      });
-      await driver.findElement(By.css('#doc-store_info')).click();
-      await driver.findElement(By.css('#doc-defaults')).click();
-      await driver.findElement(By.css('#doc-email')).click();
-      await driver.findElement(By.css('#doc-images')).click();
-      await driver.findElement(By.css('#doc-advanced')).click();
-      await driver.findElement(By.css('#doc-security')).click();
-      //check Settings and its children
-      await driver.findElement(By.css('#app-slides')).click();
-      //check Tax and its children
-      await driver.findElement(By.css('#app-tax')).click();
-      await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=tax&doc=tax_rates'), 3000).catch(function(error){
-          console.log(error);
-      });
-      await driver.findElement(By.css('#doc-tax_rates')).click();
-      await driver.findElement(By.css('#doc-tax_classes')).click();
-       //check Translations and its children
-       await driver.findElement(By.css('#app-translations')).click();
-       await driver.wait(until.urlIs('http://localhost/litecart/admin/?app=translations&doc=search'), 3000).catch(function(error){
-           console.log(error);
-       });
-       await driver.findElement(By.css('#app-users')).click();
-       await driver.findElement(By.css('#app-vqmods')).click();
-      
-})
-
 });
